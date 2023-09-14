51c6157ac8de1a166b0db4982d5069e530325a21
Olek-Nowak <ANowak001@onet.pl>
2023-09-14 14:51:17 +0200

    Added support for most commonly used arithmetic operations. Fixed bug causing skipping instruction lines

--- resource/main.asm
+++ resource/main.asm
@@ -1,4 +1,6 @@
 FIRST LINE
 _declspec(align(16)) float v_pfArrayA[] = {100.0, 12.3, 524.5, 102.9}
 movaps xmm0, v_pfArrayA
-TEST LINE
+movss xmm1, xmm0
+divps xmm0, v_pfArrayA
+addss xmm1, xmm0
--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,11 @@ std::vector<SSE_register> backup;
 fstream instructions;
 string prevInstruction;
 string nextInstruction;
+bool browseFlag;
 
 // Main przechowuje rejestry, odczytuje z pliku, wykonuje instrukcje oraz wywołuje windowManager
 // WindowManager rysuje okno oraz pobiera inputy użytkownika
+// MemoryManager przechowuje wartości odkładane na stosie
 // Rejestry przechowują wartości i wykonują podstawowe operacje / konwersje
 
 void setup() {
@@ -28,6 +30,7 @@ void setup() {
     backup = xmm;
     prevInstruction = "";
     nextInstruction = "";
+    browseFlag = 0;
 
 }
 
@@ -52,6 +55,7 @@ void parseInstruction() {
     getline(instructions, nextInstruction);
     std::vector<std::string> parsed = splitString(nextInstruction, ' ');
     // Rozpatrz każdą obsługiwaną instrukcję
+    // Deklaracja zmiennej
     if(parsed[0] == "_declspec(align(16))" && parsed[1] == "float" && parsed[3] == "=") {
         parsed[4].erase(parsed[4].begin());
         SSE_register newreg = SSE_register();
@@ -66,7 +70,9 @@ void parseInstruction() {
         //nextInstruction = parsed[4] + "/" + parsed[5] + "/" + parsed[6] + "/" + parsed[7];
 
     }
-    else if(parsed[0] == "movaps") {
+    // Ładowanie wektorowe
+    // Wyrónianie obszaru pamięci do 16 nie jest sprawdzane - movaps równoważne movups
+    else if(parsed[0] == "movaps" || parsed[0] == "movups") {
         parsed[1].pop_back();
         char numa = parsed[1].back();
         parsed[1].pop_back();
@@ -74,12 +80,265 @@ void parseInstruction() {
         if(parsed[1] == "xmm") {
             char numb = parsed[2].back();
             parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
             if(parsed[2] == "xmm") {
                 xmm[numa - 48] = xmm[numb - 48];
 
             }
             else {
                 xmm[numa - 48] = mm->find(parsed[2] + numb);
+
+            }
+
+        }
+
+    }
+    // Ładowanie skalarne
+    else if(parsed[0] == "movss") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                xmm[numa - 48].setFloats(0, xmm[numb - 48](0));
+
+            }
+            else {
+                xmm[numa - 48].setFloats(0, mm->find(parsed[2] + numb)(0));
+
+            }
+
+        }
+
+    }
+    // Dodawanie wektorowe
+    else if(parsed[0] == "addps") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                for(int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) + xmm[numb - 48](i));
+
+            }
+            else {
+                for (int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) + mm->find(parsed[2] + numb)(i));
+
+            }
+
+        }
+
+    }
+    // Dodawanie skalarne
+    else if(parsed[0] == "addss") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) + xmm[numb - 48](0));
+
+            }
+            else {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) + mm->find(parsed[2] + numb)(0));
+
+            }
+
+        }
+
+    }
+    // Odejmowanie wektorowe
+    else if(parsed[0] == "subps") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                for(int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) - xmm[numb - 48](i));
+
+            }
+            else {
+                for (int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) - mm->find(parsed[2] + numb)(i));
+
+            }
+
+        }
+
+    }
+    // Odejmowanie skalarne
+    else if(parsed[0] == "subss") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) - xmm[numb - 48](0));
+
+            }
+            else {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) - mm->find(parsed[2] + numb)(0));
+
+            }
+
+        }
+
+    }
+    // Mnożenie wektorowe
+    else if(parsed[0] == "mulps") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                for(int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) * xmm[numb - 48](i));
+
+            }
+            else {
+                for (int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) * mm->find(parsed[2] + numb)(i));
+
+            }
+
+        }
+
+    }
+    // Mnożenie skalarne
+    else if(parsed[0] == "mulss") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) * xmm[numb - 48](0));
+
+            }
+            else {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) * mm->find(parsed[2] + numb)(0));
+
+            }
+
+        }
+
+    }
+    // Dzielenie wektorowe
+    else if(parsed[0] == "divps") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                for(int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) / xmm[numb - 48](i));
+
+            }
+            else {
+                for (int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, xmm[numa - 48](i) / mm->find(parsed[2] + numb)(i));
+
+            }
+
+        }
+
+    }
+    // Dzielenie skalarne
+    else if(parsed[0] == "divss") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) / xmm[numb - 48](0));
+
+            }
+            else {
+                xmm[numa - 48].setFloats(0, xmm[numa - 48](0) / mm->find(parsed[2] + numb)(0));
+
+            }
+
+        }
+
+    }
+    // Pierwiastek kwadratowy wektorowy
+    else if(parsed[0] == "addps") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                for(int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, sqrt(xmm[numb - 48](i)));
+
+            }
+            else {
+                for (int i = 0; i < 4; i++)
+                    xmm[numa - 48].setFloats(i, sqrt(mm->find(parsed[2] + numb)(i)));
+
+            }
+
+        }
+
+    }
+    // Pierwiastek kwadratowy skalarny
+    else if(parsed[0] == "addss") {
+        parsed[1].pop_back();
+        char numa = parsed[1].back();
+        parsed[1].pop_back();
+        // Celem musi być rejestr
+        if(parsed[1] == "xmm") {
+            char numb = parsed[2].back();
+            parsed[2].pop_back();
+            // Żródłem może być rejestr lub zmienna
+            if(parsed[2] == "xmm") {
+                xmm[numa - 48].setFloats(0, sqrt(xmm[numb - 48](0)));
+
+            }
+            else {
+                xmm[numa - 48].setFloats(0, sqrt(mm->find(parsed[2] + numb)(0)));
 
             }
 
@@ -107,16 +366,21 @@ void handleInput() {
             for (int i = 0; i < backup.size(); i++)
                 wm->updateRegister(i, backup[i], backup[i]);
             wm->show();
+            browseFlag = 1;
             break;
         case sf::Keyboard::Right:
             // next step
-            parseInstruction();
+            if(browseFlag == 0) {
+                parseInstruction();
+
+            }
             wm->clear();
             wm->addHeaders();
             wm->updateInstruction(nextInstruction);
             for (int i = 0; i < xmm.size(); i++)
                 wm->updateRegister(i, backup[i], xmm[i]);
             wm->show();
+            browseFlag = 0;
             break;
         case sf::Keyboard::Space:
             // debug
